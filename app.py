7cea9bf567eeb8b3cb95d6c578a959c98ae7c05e
Ops <ops@domain.com>
2024-10-29 23:30:42 +0100

    Update api

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from api.create_clusters import create_clusters
 from api.classify_record import classify_record
+from api.multi_user_analysis import multi_user_analysis
+from api.get_user_connections import get_user_connections
 
 # Initialize FastAPI app
 app = FastAPI()
@@ -8,3 +10,5 @@ app = FastAPI()
 # Include the routers
 app.include_router(create_clusters)
 app.include_router(classify_record)
+app.include_router(multi_user_analysis)
+app.include_router(get_user_connections)
